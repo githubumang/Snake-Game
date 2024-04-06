bbb82ad72fea4589ae66518973d85da5ebcdde85
Umang <umanggg100@gmail.com>
2024-04-06 18:05:19 +0530

    fix music issue

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 // Game const and variables
 let inputDir = {x:0, y:0};
 const foodSound = new Audio('./music/food.mp3');
-const gameOverSound = new Audio('./music/gameOver.mp3');
+const gameOverSound = new Audio('./music/gameover.mp3');
 const moveSound = new Audio('./music/move.mp3');
 const musicSound = new Audio('./music/music.mp3');
 let score = 0;
